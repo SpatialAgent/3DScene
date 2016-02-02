c595faf37e51735f3f78706581747c2f83783632
Sajit Thomas <sthomas@esri.com>
2016-02-02 11:33:54 -0500

    3DScene initial commit

---
+++ resources/configurationPanel.js
@@ -0,0 +1,49 @@
+{
+  "configurationSettings": [{
+    "category": "<b>App Settings</b>",
+    "fields": [{
+      "type": "webscene",
+      "label": "Select a scene"
+    }, {
+      "type": "string",
+      "fieldName": "title",
+      "label": "Title",
+      "tooltip": "Title",
+      "placeHolder": "Title"
+    }, {
+      "type": "string",
+      "fieldName": "subtitle",
+      "label": "Subtitle",
+      "tooltip": "Subtitle",
+      "placeHolder": "Subtitle"
+    }, {
+      "type": "color",
+      "fieldName": "color",
+      "label": "Color",
+      "tooltip": "Color"
+    }, {
+      "type": "boolean",
+      "fieldName": "reverse",
+      "label": "Dark theme for map",
+      "tooltip": "Dark theme for map"
+    }, {
+      "type": "boolean",
+      "fieldName": "showLayers",
+      "label": "Enable a Layer List",
+      "tooltip": "Enable a Layer List"
+    }, {
+      "type": "boolean",
+      "fieldName": "showBasemaps",
+      "label": "Enable Basemap Selector",
+      "tooltip": "Enable Basemap Selector"
+    }]
+  }],
+  "values": {
+    "title": "",
+    "subtitle": "",
+    "color": "#2cbade",
+    "reverse": true,
+    "showLayers": true,
+    "showBasemaps": false
+  }
+}
